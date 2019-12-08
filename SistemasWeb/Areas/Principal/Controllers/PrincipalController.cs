a5e00afb2f1a0865fc21e0f6e137cac1ee8ccfa7
RaulLH <raullopezhernando@gmail.com>
2019-12-08 18:10:48 +0100

    Agregar archivos de proyecto.

---
+++ SistemasWeb/Areas/Principal/Controllers/PrincipalController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemasWeb.Areas.Principal.Controllers
+{
+    // Para poder utilizar el controlador hay que especificarle que se encuentra en la area principal
+    // Esto se realiza con el siguiente atributo
+
+   [Area("Principal")]
+    public class PrincipalController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
