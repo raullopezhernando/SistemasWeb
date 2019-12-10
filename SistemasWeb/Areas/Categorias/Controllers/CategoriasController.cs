d78339ca6033b4d8086b5183122d377b679e8aac
RaulLH <raullopezhernando@gmail.com>
2019-12-10 20:12:33 +0100

    Ultimos cambios

--- SistemasWeb/Areas/Categorias/Controllers/CategoriasController.cs
+++ SistemasWeb/Areas/Categorias/Controllers/CategoriasController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SistemasWeb.Areas.Categorias.Models;
+using SistemasWeb.Library;
 
 namespace SistemasWeb.Areas.Categorias.Controllers
 {
@@ -10,7 +13,8 @@ namespace SistemasWeb.Areas.Categorias.Controllers
     public class CategoriasController : Controller
     {
         private TCategoria _categoria;
-        private LCategoria _lcategoria;
+        private LCategorias _lcategoria;
+        // Clase que maneja los inicios de sesion de la aplicaion
         private SignInManager<IdentityUser> _signInManager;
         private static DataPaginador<TCategoria> models;
         public IActionResult Categoria()
---
+++ SistemasWeb/Areas/Categorias/Models/DataPaginador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemasWeb.Areas.Categorias.Models
+{
+    // Esta clase al estar en "Models" es accesible desde todos los lugares
+    // Aplicandole la letra "T" hacemos que la clase sea generica (
+    public class DataPaginador<T>
+    {
+        public List<T> List { get; set; }
+        public string Pagi_info { get; set; }
+        public string Pagi_navegacion { get; set; }
+        public T input { get; set; }
+        public int Registros { get; set; }
+        public string Search { get; set; }
+        public string ErrorMessage { get; set; }
+
+
+    }
+}
