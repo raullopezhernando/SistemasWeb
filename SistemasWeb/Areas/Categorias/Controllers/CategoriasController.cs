2179f1ed4f125dcbc47aa544e8df5aaa03b5b9c0
RaulLH <raullopezhernando@gmail.com>
2019-12-10 20:00:00 +0100

    Ultimos Cambios

--- SistemasWeb/Areas/Categorias/Controllers/CategoriasController.cs
+++ SistemasWeb/Areas/Categorias/Controllers/CategoriasController.cs
@@ -9,6 +9,10 @@ namespace SistemasWeb.Areas.Categorias.Controllers
     [Area("Categorias")]
     public class CategoriasController : Controller
     {
+        private TCategoria _categoria;
+        private LCategoria _lcategoria;
+        private SignInManager<IdentityUser> _signInManager;
+        private static DataPaginador<TCategoria> models;
         public IActionResult Categoria()
         {
             return View();
